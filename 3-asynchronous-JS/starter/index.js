dd721de6ddd1abd152cde0f5286bbcc5bd84271a
duyhocit <hoangduyoffline@gmail.com>
2025-09-22 15:06:39 +0700

    Requiring Modules in Practice

---
+++ 3-asynchronous-JS/starter/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+fs.readFile(`${__dirname}/dog.txt`, (error, data) => {
+    console.log("con cho " + data)
+
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+        .end((error, res) => {
+            console.log(res.body.message);
+
+            fs.writeFile('dog.txt', res.body.message, (error) => {
+                console.log("xin chao nhe")
+            })
+        });
+})
