9cd0b1ade6c7111d1635e4d5579b9ffd7d8c9cb7
duyhocit <hoangduyoffline@gmail.com>
2025-09-24 12:07:54 +0700

    building Promises

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,16 +1,48 @@
-const fs = require('fs');
-const superagent = require('superagent');
+const fs = require("fs");
+const superagent = require("superagent");
 
-fs.readFile(`${__dirname}/dog.txt`, (error, data) => {
-    console.log("con cho " + data)
+function ReadFilePro(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (error, data) => {
+      if (error) reject("i could find not file");
+      resolve(data);
+    });
+  });
+}
 
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then(res =>{
-         fs.writeFile('dog.txt', res.body.message, (error) => {
-                console.log("xin chao nhe")
-            })
-    }).catch(error =>{
-        console.log(error.message)
+const writeFile = (file,data) => {
+    return new Promise ((resovle,reject)=>{
+        fs.writeFile(file,data,(error)=>{
+            if(error) reject("cloud not file");
+        })
+        return resovle("Success")
     })
-        
-})
+}
+
+
+ReadFilePro(`${__dirname}/dog.txt`)
+  .then((res) => {
+    return superagent.get(`https://dog.ceo/api/breed/${res}/images/random`);
+  }).then((res)=>{
+    writeFile(`${__dirname}/imageDog.txt`,res.body.message);
+  })
+  .then(res =>{
+    console.log("chỉnh sửa thành công")
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+// fs.readFile(`${__dirname}/dog.txt`, (error, data) => {
+//     console.log("con cho " + data)
+
+//     superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .then(res =>{
+//          fs.writeFile('imageDog.txt', res.body.message, (error) => {
+//                 console.log("xin chao nhe")
+//             })
+//     }).catch(error =>{
+//         console.log(error.message)
+//     })
+
+// })
